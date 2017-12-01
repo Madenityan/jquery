7c030ef26cbbc73f993a512dd735bd4c9c2b5637
madenityan <marinakorsyn@gmail.com>
2017-12-01 18:14:59 +0200

    added functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+    /*--- buttons----*/
+
     $(document).ready(function() {
 
     });
@@ -30,14 +32,9 @@
         $("#button-six").trigger("click");
     });
 
-
-
-
     $("#button-eight").on("click",function(){
         $("#button-eight").clone().text("cloned").appendTo(".wrapper");
     });
-
-
 
     $("#button-nine").on("click",function(){
         console.log(
@@ -95,16 +92,79 @@
         console.log($(this).siblings());
     });
 
+    /*--- methods----*/
 
+    $("#myButton").on("click",function(){
+        console.log($(this).data("animal"));
+    });
 
+    $("#delete").on("click",function(){
+        content = $(".detach").detach();
+    });
 
+    $(document).ready(function() {
+        $("li:eq(2)").addClass("yellow");
+        $("li:eq(-4)").addClass("yellow");
+    });
 
+    $("#hasclass").on("click",function(){
+        console.log($("p").hasClass("orange"));
+    });
 
+    $("#button-html").on("click",function(){
+        $("span").html("This is a text html");
+    });
 
+    $("a").on("click",function(){
+        console.log($(this).index());
+    });
 
+    $("h3").on("click",function(){
+        $("h3").off("click");
+     });
 
+    $("input[type='checkbox']").prop(
+    "checked", true);
 
+    $("#button-remove").on("click",function(){
+        $("h4").remove();
+    });
 
+    $("#button-removeattr").on("click",function(){
+        $("#button-removeattr").removeAttr("class");
+    });
+
+     $(function(){
+        $(window).resize(function(){
+            console.log('resize');
+        });
+    });
+
+    $(function(){
+        $(window).scroll(function(){
+            console.log('scroll');
+        });
+    });
+
+    // $(function(){
+    //     $(window).scroll(function(){
+    //         if($(this).scrollTop() > 100){
+    //             $(this).scrollTop(0);
+    //         }
+    //     });
+    // });
+
+    $("#button-rslideup").on("click",function(){
+        $("#slide").slideUp();
+    });
+
+    $("#button-slidedown").on("click",function(){
+        $("#slide").slideDown();
+    });
+
+    $("#button-slidetoggle").on("click",function(){
+        $("#slide-toggle").slideToggle();
+    });
 
 
 
